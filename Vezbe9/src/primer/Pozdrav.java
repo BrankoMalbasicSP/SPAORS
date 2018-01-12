e0d01ebd5b6c018255896c5acc28d500763f772c
BrankoMalbasicSP <dmi.branko.malbasic@student.pmf.uns.ac.rs>
2018-01-12 16:59:13 +0100

    dodato obracanje

--- Vezbe9/src/primer/Pozdrav.java
+++ Vezbe9/src/primer/Pozdrav.java
@@ -2,7 +2,7 @@ package primer;
 
 public class Pozdrav {
 	public static void main(String[] args) {
-		System.out.println("Zdravo!");
+		System.out.println("Zdravo, Branko Malbasic!");
 		System.out.println("Zdravo, i tebi!");
 	}
 }
